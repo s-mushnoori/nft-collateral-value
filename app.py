b6c22faa0336d603999fc0cc0ed856e6a0196751
Sriharsha Mushnoori <sriharsha91@gmail.com>
2022-04-15 22:18:25 +0530

    added line: server = app.server

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from datetime import date
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 # Read data
 df = pd.read_csv("Data/combined_nft_cv.csv")
